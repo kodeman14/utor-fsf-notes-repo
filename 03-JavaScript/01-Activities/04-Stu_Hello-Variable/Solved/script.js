6e1c339361ff007c6de492b85d51931a3defad6f
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2024-01-11 18:00:27 -0500

    js day1 solved

---
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Solved/script.js
@@ -0,0 +1,19 @@
+// Declare variables using var
+var personName = 'Sakura';
+var pets = 3;
+var funFact = 'I like pineapple on my pizza.';
+
+// Use + to combine data and variable names in a single console log
+console.log('My name is ' + personName + '.');
+console.log('I have ' + pets + ' pet(s).');
+console.log('Fun fact: ' + funFact);
+
+// When re-assigning variables, use variable name
+personName = 'Mateo';
+pets = 5;
+funFact = 'I was a yo-yo champ in third grade.';
+
+// Logs message with re-assigned values
+console.log('My name is ' + personName + '.');
+console.log('I have ' + pets + ' pet(s).');
+console.log('Fun fact: ' + funFact);
